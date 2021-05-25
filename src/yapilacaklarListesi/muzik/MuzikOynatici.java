70e8258cb2edb1f506fecc894909622270db491d
realsarius <tehadro@protonmail.com>
2021-05-25 21:06:19 +0300

    update

--- src/yapilacaklarListesi/muzik/MuzikOynatici.java
+++ src/yapilacaklarListesi/muzik/MuzikOynatici.java
@@ -2,9 +2,9 @@ package yapilacaklarListesi.muzik;
 
 public class MuzikOynatici {
 
-    private static OKMuzik okMuzik;
-    private static CANCELMuzik cancelMuzik;
-    private static DialogMuzik dialogMuzik;
+    private static OKMuzik okMuzik = new OKMuzik();
+    private static CANCELMuzik cancelMuzik = new CANCELMuzik();
+    private static DialogMuzik dialogMuzik = new DialogMuzik();
 
     private MuzikOynatici(){
         this.okMuzik = new OKMuzik();
--- src/yapilacaklarListesi/pomodoro/model/Pomodoro.java
+++ src/yapilacaklarListesi/pomodoro/model/Pomodoro.java
@@ -4,11 +4,13 @@ import java.util.concurrent.TimeUnit;
 
 public class Pomodoro {
     private int kalanSaniye;
+    private static int staticKalanSaniye;
     private final PomodoroEnum pomodoroEnum; // final = sadece bir kez atanabilir. 25dk pomodoro. 5dk dinlen.
 
     public Pomodoro(PomodoroEnum pomodoroEnum) {
         this.pomodoroEnum = pomodoroEnum;
         kalanSaniye = pomodoroEnum.getToplamSaniye();
+        staticKalanSaniye = pomodoroEnum.getToplamSaniye();
     }
 
     public PomodoroEnum getPomodoroEnum() {
@@ -19,11 +21,15 @@ public class Pomodoro {
         kalanSaniye--;
     }
 
+    public int getKalanSaniye() {
+        return kalanSaniye;
+    }
+
     @Override
     public String toString() {
         // Pomodoro 25 dk yani Timer'ı 1500 saniye, ancak dakika:saniye olarak burada gösteriyoruz.
-        long minute = TimeUnit.SECONDS.toMinutes(this.kalanSaniye) - (TimeUnit.SECONDS.toHours(this.kalanSaniye)* 60);
-        long second = TimeUnit.SECONDS.toSeconds(this.kalanSaniye) - (TimeUnit.SECONDS.toMinutes(this.kalanSaniye) *60);
+        long minute = TimeUnit.SECONDS.toMinutes(this.kalanSaniye) - (TimeUnit.SECONDS.toHours(this.kalanSaniye) * 60);
+        long second = TimeUnit.SECONDS.toSeconds(this.kalanSaniye) - (TimeUnit.SECONDS.toMinutes(this.kalanSaniye) * 60);
         System.out.println(minute + ":" + second); // 25:00 | 24:59
         return minute + ":" + second;
     }
